eb05e24649783b491fd098c11617dd9603ab7ab9
risingPhoenix7 <achinthyahebbar@gmail.com>
2022-11-07 11:21:32 +0530

    improved efficiency of data handling in events screen

--- lib/screens/events/view_model/misc_events_view_model.dart
+++ lib/screens/events/view_model/misc_events_view_model.dart
@@ -7,9 +7,12 @@ import '../repository/retrofit/getMiscEvents.dart';
 
 class MiscEventsViewModel {
   static String? error;
-  static List<MiscEventData>? miscEventList = <MiscEventData>[];
+  static List<MiscEventData> miscEventList = <MiscEventData>[];
+  static Map<int, List<int>> miscEventSortedMap = {}; //19:[1,3,4,6],20:[]....
 
   Future<void> retrieveMiscEventResult() async {
+    miscEventList = <MiscEventData>[];
+    miscEventSortedMap = {};
     List<MiscEventCategory>? miscEventCategoryList;
     final dio = Dio();
     error = null;
@@ -17,50 +20,60 @@ class MiscEventsViewModel {
     final client = MiscEventRestClient(dio);
 
     miscEventCategoryList = await client.getAllMiscEvents().then((it) {
-
-
       return it;
     }).catchError((Object obj) {
       try {
-
         final res = (obj as DioError).response;
         error = res?.statusCode.toString();
         if (res?.statusCode == null || res == null) {
           error = ErrorMessages.noInternet;
         } else {
-
           error = matchesErrorResponse(res.statusCode, res.statusMessage);
-
         }
       } catch (e) {
         error = ErrorMessages.unknownError;
       }
       return miscEventCategoryList ?? <MiscEventCategory>[];
     });
-
+    int indexPosition = 0;
     for (MiscEventCategory miscEventCategory in miscEventCategoryList) {
       if (miscEventCategory.events != null) {
-        miscEventList!.addAll(miscEventCategory.events!);
-      }
-      // miscEventList.sort((a,b)=>{
-      //   if(a.time=='')
-      // });
-    }
+        for (MiscEventData miscevent in miscEventCategory.events!) {
+          String a = miscevent.date_time ?? '2022-11-23T19:42:24z';
 
+          try {
+            var p = DateTime.parse(a);
+            if (!(miscEventSortedMap.containsKey(p.day))) {
+              miscEventSortedMap.putIfAbsent(p.day, () => [indexPosition]);
+            } else {
+              miscEventSortedMap.update(p.day, (oldList) {
+                oldList.add(indexPosition);
+                return oldList;
+              });
+            }
+          } catch (e) {
+            //TBA
+            if (!(miscEventSortedMap.containsKey(19))) {
+              miscEventSortedMap.putIfAbsent(19, () => [indexPosition]);
+            } else {
+              miscEventSortedMap.update(19, (oldList) {
+                oldList.add(indexPosition);
+                return oldList;
+              });
+            }
+          }
+          indexPosition++;
+          miscEventList.add(miscevent);
+        }
+      }
+    }
   }
 
   List<MiscEventData> retrieveDayMiscEventData(int day_no) {
     List<MiscEventData> assortedMiscEventList = [];
-    for (int i = 0; i < miscEventList!.length; i++) {
-      String a = miscEventList![i].date_time ?? '2022-11-23T19:42:24z';
-
-      try {
-        DateTime.parse(a);
-        if (day_no == DateTime.parse(a).day) {
-          assortedMiscEventList.add(miscEventList![i]);
-        }
-      } catch (e) {
-        if (day_no == 19) assortedMiscEventList.add(miscEventList![i]);
+    if (miscEventSortedMap.containsKey(day_no)) {
+      for (var element in miscEventSortedMap[day_no] ?? []) {
+        assortedMiscEventList.add(miscEventList[element]);
       }
     }
     return assortedMiscEventList;

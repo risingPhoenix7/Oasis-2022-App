a4d260efd7cb613f294c1fe6a9f63d7b7472c59d
risingPhoenix7 <achinthyahebbar@gmail.com>
2022-11-07 21:26:38 +0530

    updated navbar

--- lib/screens/events/view/miscellaneous_screen.dart
+++ lib/screens/events/view/miscellaneous_screen.dart
@@ -5,7 +5,6 @@ import 'package:oasis_2022/resources/resources.dart';
 import 'package:oasis_2022/utils/oasis_text_styles.dart';
 
 import '/utils/scroll_remover.dart';
-import '/utils/ui_utils.dart';
 import '/widgets/error_dialogue.dart';
 import '/widgets/loader.dart';
 import '../repository/model/miscEventResult.dart';
@@ -129,81 +128,85 @@ class _EventsScreenState extends State<EventsScreen> {
                           padding: EdgeInsets.only(
                               top: 52.h, bottom: 27.5, left: 36.w, right: 36.w),
                           child: Container(
-                            height: 50.h,
-                            // width: UIUtills()
-                            //     .getProportionalWidth(width: 388),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.r),
-                              border: Border.all(
-                                  color: Color.fromRGBO(248, 216, 72, 0.45),
-                                  width: 0.5),
-                              gradient: LinearGradient(
-                                  begin: Alignment.centerLeft,
-                                  end: Alignment.centerRight,
-                                  colors: [
-                                    Color.fromRGBO(164, 108, 0, 0.15),
-                                    Color.fromRGBO(209, 154, 8, 0.15)
-                                  ]),
-                            ),
-                            child: Center(
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.only(
-                                        left: 15.0.w, right: 15.w),
-                                    child: Center(
-                                        child: SvgPicture.asset(
-                                            ImageAssets.searchIcon)),
-                                  ),
-                                  Expanded(
-                                    child: Center(
-                                      child: TextField(
-                                        onChanged: updateSearchList,
-                                        focusNode: focusNode,
-                                        controller: searchController,
-                                        //textAlignVertical: TextAlignVertical.center,
-                                        style: OasisTextStyles.openSans300
-                                            .copyWith(
-                                                fontWeight: FontWeight.w400,
-                                                color: Colors.white,
-                                                fontSize: 16.sp),
-                                        cursorColor: Colors.white,
-                                        decoration: InputDecoration(
-                                          contentPadding: EdgeInsets.symmetric(
-                                              vertical: 14.8.h),
-                                          border: InputBorder.none,
-                                          // labelStyle:
-                                          // OasisTextStyles.openSans300.copyWith(fontWeight: FontWeight.w400,color: Color(0xFFC0C0C0),fontSize: 16.sp),
-                                          hintText: "Search for events...",
-                                          hintStyle: OasisTextStyles.openSans300
+                            color: Colors.black,
+                            child: Container(
+                              height: 50.h,
+                              // width: UIUtills()
+                              //     .getProportionalWidth(width: 388),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.r),
+                                border: Border.all(
+                                    color: Color.fromRGBO(248, 216, 72, 0.45),
+                                    width: 0.5),
+                                gradient: LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.centerRight,
+                                    colors: [
+                                      Color.fromRGBO(164, 108, 0, 0.15),
+                                      Color.fromRGBO(209, 154, 8, 0.15)
+                                    ]),
+                              ),
+                              child: Center(
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                          left: 15.0.w, right: 15.w),
+                                      child: Center(
+                                          child: SvgPicture.asset(
+                                              ImageAssets.searchIcon)),
+                                    ),
+                                    Expanded(
+                                      child: Center(
+                                        child: TextField(
+                                          onChanged: updateSearchList,
+                                          focusNode: focusNode,
+                                          controller: searchController,
+                                          //textAlignVertical: TextAlignVertical.center,
+                                          style: OasisTextStyles.openSans300
                                               .copyWith(
                                                   fontWeight: FontWeight.w400,
-                                                  color: Color(0xFFC0C0C0),
+                                                  color: Colors.white,
                                                   fontSize: 16.sp),
-                                          suffixIcon: IconButton(
-                                              onPressed: () {
-                                                searchController.clear();
+                                          cursorColor: Colors.white,
+                                          decoration: InputDecoration(
+                                            contentPadding:
+                                                EdgeInsets.symmetric(
+                                                    vertical: 14.8.h),
+                                            border: InputBorder.none,
+                                            // labelStyle:
+                                            // OasisTextStyles.openSans300.copyWith(fontWeight: FontWeight.w400,color: Color(0xFFC0C0C0),fontSize: 16.sp),
+                                            hintText: "Search for events...",
+                                            hintStyle: OasisTextStyles
+                                                .openSans300
+                                                .copyWith(
+                                                    fontWeight: FontWeight.w400,
+                                                    color: Color(0xFFC0C0C0),
+                                                    fontSize: 16.sp),
+                                            suffixIcon: IconButton(
+                                                onPressed: () {
+                                                  searchController.clear();
 
-                                                focusNode.unfocus();
-                                                setState(() {});
-                                              },
-                                              icon: SvgPicture.asset(
-                                                  ImageAssets.crossIcon)),
+                                                  focusNode.unfocus();
+                                                  setState(() {});
+                                                },
+                                                icon: SvgPicture.asset(
+                                                    ImageAssets.crossIcon)),
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
                             ),
                           ),
                         ),
                         Container(
                           child: Padding(
-                            padding:
-                                EdgeInsets.fromLTRB(36.w, 27.h, 36.w, 0.h),
+                            padding: EdgeInsets.fromLTRB(36.w, 27.h, 36.w, 0.h),
                             child: Row(
                                 mainAxisAlignment:
                                     MainAxisAlignment.spaceBetween,
